4077451045ccb9326b22f9d196e37cfb48821feb
tesfaye eshetie <tesfayeeshetie19@gmail.com>
2022-03-21 22:11:33 -0400

    initial commit

---
+++ src/com/tesfayeeshetie/Music.java
@@ -0,0 +1,4 @@
+package com.tesfayeeshetie;
+
+public class Music {
+}
---
+++ src/com/tesfayeeshetie/Zipcode.java
@@ -0,0 +1,9 @@
+package com.tesfayeeshetie;
+
+public class Zipcode {
+    String zipcode;
+
+    public Zipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+}
